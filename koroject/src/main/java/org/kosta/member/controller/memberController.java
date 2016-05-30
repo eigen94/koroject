8afe5b856ca2045731035019010732df86146e47
Oh <kosta-23@kosta-23-PC.mshome.net>
2016-05-30 14:55:12 +0900

    x

--- koroject/src/main/java/org/kosta/member/controller/memberController.java
+++ koroject/src/main/java/org/kosta/member/controller/memberController.java
@@ -5,17 +5,30 @@ import javax.inject.Inject;
 import org.kosta.member.service.MemberService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/member/*")
 public class memberController {
 
 	private static final Logger logger = LoggerFactory.getLogger(memberController.class);
 	
 	@Inject
 	private MemberService service;
+
+	@Autowired
+	public void setService(MemberService service) {
+		this.service = service;
+	}
 	
+	@RequestMapping(value="insert_member",method=RequestMethod.GET)
+	public String inert_form(Model model){
+		model.addAttribute("title", "¸â¹ö");
+		
+		return "/memberRegister/insertMember";
+	}
 	
 }
