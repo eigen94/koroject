72cfb7913a1c24d9811ccbca3973be277cf2893d
cyc2133 <cyc2133@naver.com>
2016-06-03 16:23:29 +0900

    왠지모를 메신저에러 제거함

--- koroject/src/main/webapp/WEB-INF/views/messenger/socket.room.js
+++ koroject/src/main/webapp/WEB-INF/views/messenger/socket.room.js
@@ -18,14 +18,16 @@ var io = require('socket.io').listen(server);
 
 io.sockets.on( 'connection', function(socket){
 	socket.on( 'join', function(data){
-		/*test*/console.log(data) // data : ����ڰ� �Է��� ���̸�
-		socket.join(data); //����ڰ� �Է��� �濡 socket�� ������Ų��.
-		socket.room = data; //'room' �Ӽ��� ����ڰ� �Է��� ���̸��� �����Ѵ�.
+		console.log(data) // 방 이름이 무엇인고 ? 
+		socket.join(data); 	//입력한 방 이름으로 접속 !! 
+		socket.room = data; 
 	});
 	
 	socket.on( 'message', function(data){
-		//'room' �Ӽ����� �ش��ϴ� �濡 �������� Client�� �޼����� ������.
-		console.log( 'id : %s, msg : %s, date : %s', data.id, data.message, data.date );
-		io.sockets.in( socket.room ).emit('message', data); //public ��� : io.sockets.emit(...);
+		console.log( 'id : %s, msg : %s', data.id, data.message );
+//		console.log( 'id : %s, msg : %s, date : %s', data.id, data.message, data.date );
+		
+		//같은 방에 접속한 사용자들에게 메세지를 뿌림 
+		io.sockets.in( socket.room ).emit('message', data); 
 	});
 });
