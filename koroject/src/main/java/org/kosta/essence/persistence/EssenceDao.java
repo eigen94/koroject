574bddcb71b9f5e0cd989d09fd5ceb3fb1fd1506
Dinky <Dinky@172.16.43.1>
2016-06-18 15:32:48 +0900

    에센스 몽고DB이전완료

--- koroject/src/main/java/org/kosta/essence/persistence/EssenceDao.java
+++ koroject/src/main/java/org/kosta/essence/persistence/EssenceDao.java
@@ -1,29 +1,43 @@
 package org.kosta.essence.persistence;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class EssenceDao {
-//org.kosta.mapper.EssenceMapper
-	@Inject	private SqlSession session;
-	private static String namespace = "org.kosta.mapper.EssenceMapper";
+	@Inject private MongoTemplate mongo;
 	
-	public String insert(int p_id, String json) {
-		HashMap map = new HashMap();
-		map.put("p_id", p_id);
-		map.put("json", json);
-		session.insert(namespace + ".insert", map);
+	public String update(Map<String, String> map){
+		Criteria criteria = new Criteria("id");
+		criteria.is(map.get("id"));
+		
+		Query query = new Query(criteria);
+		Update update = new Update();
+		update.set("content", map.get("content"));
+		mongo.updateFirst(query, update, "essence");	
+		
 		return null;
 	}
-
+	
 	public String load(int p_id) {
 		// TODO Auto-generated method stub
-		return session.selectOne(namespace+".load",p_id);
+		//return session.selectOne(namespace+".load",p_id);
+		Criteria cri = new Criteria("id");
+		cri.is(Integer.toString(p_id));
+		Query query = new Query(cri);
+				
+		Map<String,String> map = mongo.findOne(query, HashMap.class, "essence");
+		String json = map.get("content");		
+		return json;
 	}
 
 }
--- koroject/src/main/java/org/kosta/projectBoard/persistence/ProjectBoardDAOImpl.java
+++ koroject/src/main/java/org/kosta/projectBoard/persistence/ProjectBoardDAOImpl.java
@@ -1,24 +1,32 @@
 package org.kosta.projectBoard.persistence;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
 import org.kosta.member.domain.Member;
 import org.kosta.projectBoard.domain.ProjectBoard;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ProjectBoardDAOImpl implements ProjectBoardDAO {
 
 	@Inject	private SqlSession session;
+	@Inject private MongoTemplate mongo;
 	
 	private static String namespace = "org.kosta.mapper.ProjectBoardMapper";
 	
 	@Override
 	public void create(ProjectBoard pb) {
 		session.insert(namespace+".create", pb);		
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("id", Integer.toString(session.selectOne(namespace+".getLastIndex")));
+		map.put("json",null);
+		mongo.insert(map, "essence");
 	}
 
 	@Override
